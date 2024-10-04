14989601f58baaa4468e218b6bb3eb36c3307b0f
gsulay <sulaygifrey@gmail.com>
2024-10-04 19:31:32 +0800

    deleted error

--- gold-station/charcoalRequest.lua
+++ gold-station/charcoalRequest.lua
@@ -1,7 +1,7 @@
 -- requestItem = require("requestItem")
 -- local station_name = "gold-station"
 -- charcoalInventory = ""
---edit pair 6
+--edit pair 7
 
 function charcoalRun(charcoalInventory, station_name, requestItem, side)
     rednet.open(side)
@@ -41,7 +41,7 @@ function charcoalRun(charcoalInventory, station_name, requestItem, side)
                 itemDetails = charcoalWrapped.list()
                 curCharcoal = 0
         
-                for i=1, #4 do
+                for i=1, 4 do
                     curItem = charcoalWrapped.getItemDetail(i)
                     if curItem ~= nil then
                         if curItem["name"] == "minecraft:charcoal" then
